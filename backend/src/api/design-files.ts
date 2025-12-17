34e41fbc2bbacc25d77f10d9dce7d6ea7ec85b6a
Suleiman Leadbitter <sulcalibur@me.com>
2025-12-17 19:43:49 +0000

    general openspec update

--- backend/src/api/design-files.ts
+++ backend/src/api/design-files.ts
@@ -1,4 +1,4 @@
-import { FastifyPluginAsync } from 'fastify'
+import type { FastifyPluginAsync } from 'fastify'
 import { DesignFileRepository } from '../domain/repositories/DesignFileRepository'
 import { initDataSource } from '../infrastructure/database/data-source'
 
