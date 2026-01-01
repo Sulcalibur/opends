a57cb2477f54d0ca38c0c2ec6088c379d0e21f25
Suleiman Leadbitter <sulcalibur@me.com>
2026-01-01 23:03:10 +0000

    Remove development-only files from working directory

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "admin" | "centered" | "default"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
